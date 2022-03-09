9d9f1a17ef8e14321233e602c6dc9de9750ecc62
Andres <andresogando10@gmail.com>
2022-03-08 23:05:49 -0500

    base scripts added

--- jobs-api/src/jobs/jobs.service.ts
+++ jobs-api/src/jobs/jobs.service.ts
@@ -1,20 +1,21 @@
-import { CacheStore, CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
-import { PaginationQueryDto, TechnologyQueryDto } from 'src/common';
-import { LocationQueryDto } from 'src/common/dto/location-query.dto';
-import { DynamoService } from 'src/db';
+import { CacheStore, CACHE_MANAGER, Inject, Injectable } from "@nestjs/common";
+import { PaginationQueryDto, TechnologyQueryDto } from "src/common";
+import { LocationQueryDto } from "src/common/dto/location-query.dto";
+import { DynamoService } from "src/db";
 
 @Injectable()
 export class JobsService {
   constructor(
     @Inject(CACHE_MANAGER) private cacheManager: CacheStore,
-    private readonly client: DynamoService,
+    private readonly client: DynamoService
   ) {}
 
   async findAll(paginationQuery: PaginationQueryDto) {
     const { limit, sort } = paginationQuery;
-    const cacheKey = limit + '_' + sort;
-    const value = await this.cacheManager.get(cacheKey);
-    if (value) {
+    const cacheKey = limit + "_" + sort;
+    const value: any = await this.cacheManager.get(cacheKey);
+
+    if (value && value.data.lenght > 0) {
       return value;
     }
 
@@ -25,11 +26,13 @@ export class JobsService {
 
   async findByLocation(locationQuery: LocationQueryDto) {
     const { limit, location, sort } = locationQuery;
-    const cacheKey = location + '_' + limit + '_' + sort;
-    const value = await this.cacheManager.get(cacheKey);
-    if (value) {
+    const cacheKey = location + "_" + limit + "_" + sort;
+    const value: any = await this.cacheManager.get(cacheKey);
+
+    if (value && value.data.length > 0) {
       return value;
     }
+
     const response = await this.client.findByLocation(locationQuery);
     await this.cacheManager.set(cacheKey, response, { ttl: 60 * 10 });
     return response;
@@ -37,10 +40,10 @@ export class JobsService {
 
   async findByTechnology(technologyQuery: TechnologyQueryDto) {
     const { technology, limit, sort } = technologyQuery;
-    const cacheKey = technology + '_' + limit + '_' + sort;
-    const value = await this.cacheManager.get(cacheKey);
+    const cacheKey = technology + "_" + limit + "_" + sort;
+    const value: any = await this.cacheManager.get(cacheKey);
 
-    if (value) {
+    if (value && value.data.length > 0) {
       return value;
     }
 
